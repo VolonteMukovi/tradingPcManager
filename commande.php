281cd2286959212565d24afe4bc3e4ce652ba52a
volonteMukovi <mukovivolonte@gmail.com>
2024-08-27 00:09:13 +0200

    demain se mardi je dorm

--- commande.php
+++ commande.php
@@ -3,7 +3,7 @@ include "connexion_db.php";
 function loginAdmin($db, $pseudo, $pwd)
 {
     try {
-        $req = $db->query("SELECT * FROM `tb_admin` WHERE `pseudo_admin`='".$pseudo."' AND `mdp_admin`='".$pwd."'");
+        $req = $db->query("SELECT * FROM `tb_admin` WHERE `pseudo_admin`='" . $pseudo . "' AND `mdp_admin`='" . $pwd . "'");
         if ($req->rowCount() > 0) {
             $data = $req->fetchAll(PDO::FETCH_OBJ);
             setcookie("admin", serialize($data), time() + (365 * 24 * 60 * 60));
@@ -28,18 +28,18 @@ function editAdmin($db, $pseudo, $pwd)
 
 
 // ======================================================= CATEGORIE ===================================================================
-function insertCategorie($db,$nom_categorie,$description)
+function insertCategorie($db, $nom_categorie, $description)
 {
     try {
         $req = $db->prepare("INSERT INTO `tb_categorie`(`nom_categorie`, `Description`) VALUES (?,?)");
-        $req->execute(array($nom_categorie,$description));
+        $req->execute(array($nom_categorie, $description));
         header("location: ajout_categorie.php");
     } catch (Exception $e) {
         $e->getMessage();
     }
 }
 
-function AfficheAdmin($db)
+function AfficheCategorie($db)
 {
     try {
         $req = $db->query("SELECT * FROM `tb_categorie`");
@@ -50,11 +50,22 @@ function AfficheAdmin($db)
     }
 }
 
-function editCategorie($db,$id_categorie,$nom_categorie,$description)
+function AfficheCategoriEdit($db)
+{
+    try {
+        $req = $db->query("SELECT * FROM `tb_categorie`");
+        $data = $req->fetchAll(PDO::FETCH_OBJ);
+        return $data;
+    } catch (Exception $e) {
+        $e->getMessage();
+    }
+}
+
+function editCategorie($db, $id_categorie, $nom_categorie, $description)
 {
     try {
         $req = $db->prepare("UPDATE `tb_categorie` SET `nom_categorie`=?,`Description`=? WHERE `id_categorie`=?");
-        $req->execute(array($db,$nom_categorie,$description,$id_categorie));
+        $req->execute(array($db, $nom_categorie, $description, $id_categorie));
         header("location: categorie.php");
     } catch (Exception $e) {
         $e->getMessage();
@@ -62,11 +73,11 @@ function editCategorie($db,$id_categorie,$nom_categorie,$description)
 }
 
 
-function supCategorie($db,$id_categorie)
+function deleteCategorie($db, $id_categorie)
 {
     try {
         $req = $db->prepare("DELETE FROM `tb_categorie` WHERE `id_categorie`=?");
-        $req->execute(array($db,$id_categorie));
+        $req->execute(array($db, $id_categorie));
         header("location: categorie.php");
     } catch (Exception $e) {
         $e->getMessage();
@@ -75,19 +86,41 @@ function supCategorie($db,$id_categorie)
 
 // =========================================================================== ORDINATEUR ==================================================================
 
-function afficheEleveEdit($db, $nom_produit, $prixAchat_produit, $prixVente_produit, $fournisseur_prouiduit, $photos_produit, $id_categorie)
+function saveProduits($db, $nom_produit, $prixAchat_produit, $prixVente_produit, $fournisseur_prouiduit, $photo, $id_categorie)
 {
     try {
-        $annee = "Annee en cours";
-        $req = $db->prepare("INSERT INTO `tb_produits`(`nom_produit`, `prixAchat_produit`, `prixVente_produit`, `fournisseur_prouiduit`, `photos_produit`, `id_categorie`) VALUES (?,?,?,?,?)");
-        $req->execute(array($nom_produit, $prixAchat_produit, $prixVente_produit, $fournisseur_prouiduit, $photos_produit, $id_categorie));
-        header("location: clients.php");
+        if (isset($photo) and $photo['error'] == 0) {
+            if ($photo['size'] <= 1000000000000000000000000000000000000000000000000000000000000) {
+                $infosfichier = pathinfo($photo['name']);
+                $extension_upload = $infosfichier['extension'];
+                $extensions_autorisees = array('png', 'jpg', 'jpeg', "avif");
+                if (in_array($extension_upload, $extensions_autorisees)) {
+                    if (move_uploaded_file($photo['tmp_name'], './Images/' . basename($photo['name']))) {
+                        try {
+                            $req = $db->prepare("INSERT INTO `tb_produits`(`nom_produit`, `prixAchat_produit`, `prixVente_produit`, `fournisseur_prouiduit`, `photos_produit`, `id_categorie`) VALUES (?,?,?,?,?)");
+                            $req->execute(array($nom_produit, $prixAchat_produit, $prixVente_produit, $fournisseur_prouiduit,basename($photo['name']), $id_categorie));
+                            header("location: produits.php");
+                        } catch (PDOException $e) {
+                            echo $e->getMessage();
+                        }
+                    } else {
+                        echo "Votre image n'as pas pui etre envoiye au serveur veillez ressayer";
+                    }
+                } else {
+                    echo "L'extension est incorrect";
+                }
+            } else {
+                echo "Veille verifier la taille de votre image peut etre il esg grand par rapport a la taille autorise";
+            }
+        } else {
+            echo "votre image n'as pas ete envoyer au serveur ou n'as pas eter trouver";
+        }
     } catch (Exception $e) {
         $e->getMessage();
     }
 }
 
-function eleveEdit($db, $matricule_eleve, $nom_eleve, $postnom, $code, $genre_eleve, $lieuNaissance_eleve, $dateNaissance_eleve, $adress_eleve, $ecoleOrigine_eleve, $numePerma_eleve, $nomTuteur_eleve, $numeTelTuteur_eleve, $nationalite_eleve, $photo, $id_classes_inscript, $anneeScholair_inscript, $ide_eleve, $id_inscription)
+function produitsEdit($db, $nom_produit, $prixAchat_produit, $prixVente_produit, $fournisseur_prouiduit, $photo, $id_categorie)
 {
     if (isset($photo) and $photo['error'] == 0) {
         if ($photo['size'] <= 1000000000000000000000000000000000000000000000000000000000000) {
@@ -97,12 +130,10 @@ function eleveEdit($db, $matricule_eleve, $nom_eleve, $postnom, $code, $genre_el
             if (in_array($extension_upload, $extensions_autorisees)) {
                 if (move_uploaded_file($photo['tmp_name'], './Images/' . basename($photo['name']))) {
                     try {
-                        $req =  $db->prepare("UPDATE `tb_eleve` SET `Matricule_eleve`=?,`Nom_eleve`=?,`postnom`=?,`code`=?,`genre_eleve`=?,`lieuNaissance_eleve`=?,`dateNaissance_eleve`=?,`adress_eleve`=?,`ecoleOrigine_eleve`=?,`numePerma_eleve`=?,`nomTuteur_eleve`=?,`numeTelTuteur_eleve`=?,`Nationalite_eleve`=?, `Photo_eleve`=? WHERE `ID_eleve`=?");
-                        $req->execute(array($matricule_eleve, $nom_eleve, $postnom, $code, $genre_eleve, $lieuNaissance_eleve, $dateNaissance_eleve, $adress_eleve, $ecoleOrigine_eleve, $numePerma_eleve, $nomTuteur_eleve, $numeTelTuteur_eleve, $nationalite_eleve, basename($photo['name']), $ide_eleve));
-                        $req =  $db->prepare("UPDATE `tb_inscription` SET `id_classes_inscript`=?,`anneeScholair_inscript`=? WHERE `ID_inscription` =?");
-                        $req->execute(array($id_classes_inscript, $anneeScholair_inscript, $id_inscription));
+                        $req =  $db->prepare("UPDATE tb_produits SET `nom_produit`=?, `prixAchat_produit`=?, `prixVente_produit`=?, `fournisseur_prouiduit`=?, `photos_produit`=?, `id_categorie`=?");
+                        $req->execute(array($nom_produit, $prixAchat_produit, $prixVente_produit, $fournisseur_prouiduit, basename($photo['name']), $id_categorie));
                         unset($_POST);
-                        header("location: ajouts_eleves.php");
+                        header("location: produits.php");
                     } catch (PDOException $e) {
                         echo $e->getMessage();
                     }
@@ -118,19 +149,17 @@ function eleveEdit($db, $matricule_eleve, $nom_eleve, $postnom, $code, $genre_el
     } else {
 
         try {
-            $req =  $db->prepare("UPDATE `tb_eleve` SET `Matricule_eleve`=?,`Nom_eleve`=?,`postnom`=?,`code`=?,`genre_eleve`=?,`lieuNaissance_eleve`=?,`dateNaissance_eleve`=?,`adress_eleve`=?,`ecoleOrigine_eleve`=?,`numePerma_eleve`=?,`nomTuteur_eleve`=?,`numeTelTuteur_eleve`=?,`Nationalite_eleve`=? WHERE `ID_eleve`=?");
-            $req->execute(array($matricule_eleve, $nom_eleve, $postnom, $code, $genre_eleve, $lieuNaissance_eleve, $dateNaissance_eleve, $adress_eleve, $ecoleOrigine_eleve, $numePerma_eleve, $nomTuteur_eleve, $numeTelTuteur_eleve, $nationalite_eleve, $ide_eleve));
-            $req =  $db->prepare("UPDATE `tb_inscription` SET `id_classes_inscript`=?,`anneeScholair_inscript`=? WHERE `ID_inscription` =?");
-            $req->execute(array($id_classes_inscript, $anneeScholair_inscript, $id_inscription));
+            $req =  $db->prepare("UPDATE tb_produits SET `nom_produit`=?, `prixAchat_produit`=?, `prixVente_produit`=?, `fournisseur_prouiduit`=?, `id_categorie`=?");
+            $req->execute(array($nom_produit, $prixAchat_produit, $prixVente_produit, $fournisseur_prouiduit, $id_categorie));
             unset($_POST);
-            header("location: inscrits.php");
+            header("location: produits.php");
         } catch (PDOException $e) {
             echo $e->getMessage();
         }
     }
 }
 
-function deleteEleve($db, $id_eleve)
+function deleteProduits($db, $id_eleve)
 {
     try {
         $req = $db->query("DELETE FROM `tb_eleve` WHERE `ID_eleve` = '" . $id_eleve . "' ");
@@ -140,7 +169,7 @@ function deleteEleve($db, $id_eleve)
     }
 }
 
-function AfficheAnneePasserEleve($db, $annee, $status)
+function AfficheProduits($db, $annee, $status)
 {
     try {
         $req = $db->query("SELECT * FROM tb_inscription JOIN tb_eleve ON tb_inscription.id_eleve_inscript = tb_eleve.ID_eleve JOIN tb_classes ON tb_inscription.id_classes_inscript = tb_classes.ID_classes JOIN tb_annee_scholaire ON tb_inscription.anneeScholair_inscript = tb_annee_scholaire.ID_anne_scholaire JOIN tb_option ON tb_classes.id_option = tb_option.ID_option JOIN tb_section ON tb_option.id_section = tb_section.ID_section WHERE tb_annee_scholaire.annee ='" . $annee . "' AND `status`='" . $status . "'");
@@ -155,20 +184,10 @@ function countEleve($db)
 {
     try {
         $annee = "Annee en cours";
-        $req = $db->query("SELECT COUNT(*) AS nbrEleve FROM tb_inscription JOIN tb_eleve ON tb_inscription.id_eleve_inscript = tb_eleve.ID_eleve JOIN tb_classes ON tb_inscription.id_classes_inscript = tb_classes.ID_classes JOIN tb_annee_scholaire ON tb_inscription.anneeScholair_inscript = tb_annee_scholaire.ID_anne_scholaire JOIN tb_option ON tb_classes.id_option = tb_option.ID_option JOIN tb_section ON tb_option.id_section = tb_section.ID_section WHERE tb_annee_scholaire.status = '".$annee."'");
+        $req = $db->query("SELECT COUNT(*) AS nbrEleve FROM tb_inscription JOIN tb_eleve ON tb_inscription.id_eleve_inscript = tb_eleve.ID_eleve JOIN tb_classes ON tb_inscription.id_classes_inscript = tb_classes.ID_classes JOIN tb_annee_scholaire ON tb_inscription.anneeScholair_inscript = tb_annee_scholaire.ID_anne_scholaire JOIN tb_option ON tb_classes.id_option = tb_option.ID_option JOIN tb_section ON tb_option.id_section = tb_section.ID_section WHERE tb_annee_scholaire.status = '" . $annee . "'");
         $data = $req->fetchAll(PDO::FETCH_OBJ);
         return $data;
     } catch (Exception $e) {
         $e->getMessage();
     }
 }
-
-
-
-
-
-
-
-
-
-?>
